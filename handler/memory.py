dfa210c0168a8414be2007793fdf5f4de50479c2
Osama yousef <fotboy1788@hotmail.com>
2022-02-12 00:21:44 +0200

    fix path

--- handler/memory.py
+++ handler/memory.py
@@ -15,6 +15,7 @@ class Memory:
         mem_dir = os.path.realpath('.') + "\\memory\\"
         print(mem_dir)
         # setting up variables to run winpemem with different parameters
-        mem_acq_get = "winpmem_mini_x64_rc2.exe " + mem_dir + "\\memdump.raw"
+        mem_acq_get = "winpmem_mini_x64_rc2.exe " + mem_dir + "memdump.raw"
+        print(mem_acq_get)
         # executing winpmem
         subprocess.call(mem_acq_get, stderr=self.noError)
